e924e20cf418c58d792f73bbcc89518d012315ac
Marco Ramírez <marcosrueda214@gmail.com>
2024-07-27 20:05:17 -0300

    Firsst Commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+let nombre = prompt("¿Cual es tu nombre?")
+
+if(nombre){
+    alert("Holaaa "+ nombre)
+}
+
+let edad = parseFloat(prompt("Ingresa tu edad"))
+
+if(edad){
+    alert("Tu edad es "+(edad))
+}else{
+    alert("Mi loco tu eta troleando")
+}
