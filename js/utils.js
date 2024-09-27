df75514f7ff7e957178b782de40876196fbfde34
Marco Ramírez <marcosrueda214@gmail.com>
2024-09-27 05:46:22 -0300

    Final final

--- js/utils.js
+++ js/utils.js
@@ -1,22 +1,25 @@
 function obtenerTasaConversion(de, a, tasas) {
-    const tasaDe = tasas[de];
-    const tasaA = tasas[a];
-    return tasaDe && tasaA ? tasaA / tasaDe : null;
+    const tasaDe = tasas[de]
+    const tasaA = tasas[a]
+    if (tasaDe && tasaA) {
+        return tasaDe / tasaA
+    }
+    return null
 }
 
 function guardarHistorial(transaccion) {
-    const historial = JSON.parse(localStorage.getItem('historialConversiones')) || [];
-    historial.push(transaccion);
-    localStorage.setItem('historialConversiones', JSON.stringify(historial));
+    const historial = JSON.parse(localStorage.getItem('historialConversiones')) || []
+    historial.push(transaccion)
+    localStorage.setItem('historialConversiones', JSON.stringify(historial))
 }
 
 function validarCBU(cbu) {
-    return /^[0-9]{22}$/.test(cbu);
+    return /^[0-9]{22}$/.test(cbu)
 }
 
 function mostrarMensaje(container, mensaje, tipo) {
-    container.innerHTML = `<div class="alert alert-${tipo}" role="alert">${mensaje}</div>`;
+    container.innerHTML = `<div class="alert alert-${tipo}" role="alert">${mensaje}</div>`
     setTimeout(() => {
-        container.innerHTML = '';
-    }, 3000);
+        container.innerHTML = ''
+    }, 3000)
 }
